f77c4930cb0d65c67832eb0863c853c774db6619
Rayjan Wilson <rayjan.wilson@gmail.com>
2014-10-02 09:04:40 -0800

    updated linaro release

--- clone-repos.sh
+++ clone-repos.sh
@@ -3,7 +3,7 @@
 
 git clone https://github.com/jwrdegoede/u-boot-sunxi.git  -b sunxi-next
 git clone https://github.com/linux-sunxi/linux-sunxi -b sunxi-next
-wget http://releases.linaro.org/14.08/ubuntu/trusty-images/nano/linaro-trusty-nano-20140821-681.tar.gz
+wget http://releases.linaro.org/14.09/ubuntu/trusty-images/nano/linaro-trusty-nano-20140922-682.tar.gz
 git clone https://github.com/lwfinger/rtl8188eu
 git clone git://github.com/linux-sunxi/sunxi-tools.git
 git clone git://github.com/linux-sunxi/sunxi-boards.git
